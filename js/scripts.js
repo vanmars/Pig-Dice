8f0d114217054d1c1e71c95523abe5ca49091f44
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-26 10:18:24 -0700

    it will add the basic functionality for the buttons and connect the user interface to backend logic
    Co-authored-by: Vanessa Stewart <vstewart1213@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 // back-end logic
 
-function ScoreCard(){
-this.players=[];
+function ScoreCard(player1,player2){
+this.players=[player1,player2];
 }
-ScoreCard.prototype.addPlayer = function(player){
-  this.players.push(player);
-}
+//ScoreCard.prototype.addPlayer = function(player){
+//this.players.push(player);
+//}
 
 function Player(totalScore,turnScore,playerId){
   this.totalScore = totalScore;
@@ -14,8 +14,13 @@ function Player(totalScore,turnScore,playerId){
   
 }
 
+let diceRoll = function(){
+  let roll = Math.ceil(Math.random()*6);
+  return roll;
+}
+
 Player.prototype.rollDice = function(){
-  let currentRoll = diceRole();
+  let currentRoll = diceRoll();
   if(currentRoll !== 1){
     this.turnScore += currentRoll;
   }
@@ -29,16 +34,39 @@ Player.prototype.rollDice = function(){
 Player.prototype.hold = function(){
   this.totalScore += this.turnScore
   if(this.totalScore >= 100){
-    alert("you have won!!!")
+    $(".winCard").show();
+    $("span#winningPlayer").text(this.playerId)
   }
   else {
+    this.turnScore = 0;
     return this.totalScore;
   }
 }
 
-let diceRoll = function(){
-  let roll = Math.ceil(Math.random()*6);
-  return roll;
-}
+
 //user interface logic-------
 
+$(document).ready(function(){
+  let player1 = new Player(0,0,1);
+  let player2 = new Player(0,0,2);
+  let newGame= new ScoreCard(player1,player2);
+  $("button#player1Roll").click(function(){
+    let rollDice1 = player1.rollDice();
+    $("span#roll").text(rollDice1);  // Need to display individual dice rolls
+    $("span#player1TurnScore").text(rollDice1);
+  });
+  $("button#player1Hold").click(function(){
+    let hold1 = player1.hold();
+    $("span#player1totalScore").text(hold1);
+    
+  });
+  $("button#player2Roll").click(function(){
+    let rollDice2 = player2.rollDice();
+    $("span#roll").text(rollDice2);  // Need to display individual dice rolls
+    $("span#player2TurnScore").text(rollDice2);
+  });
+  $("button#player1Roll").click(function(){
+    let hold2 = player2.hold();
+    $("span#player2totalScore").text(hold2);
+  });
+});
