1d792f17477e24f6a8d05d356031defae2e40377
Daniel Schaaf <daniel.schaaf@outlook.com>
2020-08-26 16:30:11 -0700

    it will add the rules for two dice
    Co-authored-by: Vanessa Stewart <vstewart1213@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+
+
+
 // Business Logic
 function ScoreCard(player1, player2) {
   this.players = [player1, player2];
@@ -12,28 +15,29 @@ function Player(playerId) {
 
 let diceRoll = function () {
   // let roll = Math.ceil(Math.random() * 6);
-  let roll = Math.ceil(Math.random()*6);
+  let roll = Math.ceil(Math.random() * 6);
   return roll;
 }
 
 Player.prototype.rollDice = function () {
   let currentRoll = diceRoll();
+
   if (currentRoll !== 1) {
-    this.turnScore += currentRoll;
+    this.turnScore += currentRoll
   }
   else {
     this.turnScore = 0;
   };
   return currentRoll;
 };
- 
+
 
 Player.prototype.hold = function () {
   this.totalScore += this.turnScore
   this.turnScore = 0;
-  if(this.totalScore >= 100){
-   $(".winCard").show();
-   $("span#winningPlayer").text(this.playerId)
+  if (this.totalScore >= 100) {
+    $(".winCard").show();
+    $("span#winningPlayer").text(this.playerId)
   };
   return this.totalScore;
 };
@@ -48,17 +52,29 @@ let player2 = new Player(2);
 let newGame = new ScoreCard(player1, player2);
 
 let attachEventListeners = function () {
-   // Event Listener for Player 1 Roll
-   $("button#player1Roll").click(function () {
-    let roll = player1.rollDice();
-    $("span#roll").text(roll);
+  // Event Listener for Player 1 Roll
+  $("button#player1Roll").click(function () {
+    let roll1 = player1.rollDice();
+    let roll2 = player1.rollDice();
+    $("span#roll1").text(roll1);
+    $("span#roll2").text(roll2);
+
+
+
     $("span#player1TurnScore").text(player1.turnScore);
 
 
-  
 
+    if (roll1 === 1 && roll2 === 1) {
+      player1.totalScore = 0;
+      // Disable player one's buttons
+      $("button.player1Btn").prop("disabled", true);
+      // Enable player two's buttons
+      $("button.player2Btn").prop("disabled", false);
+      newGame.currentPlayer = 2;
+    }
     // If player one, rolls a one, need to disable buttons
-    if (roll === 1){
+    else if (roll1 === 1 || roll2 === 1) {
       // Disable player one's buttons
       $("button.player1Btn").prop("disabled", true);
       // Enable player two's buttons
@@ -83,12 +99,20 @@ let attachEventListeners = function () {
 
   //  Event Listener for Player 2 Roll
   $("button#player2Roll").click(function () {
-    let roll = player2.rollDice();
-    $("span#roll").text(roll);  
+    let roll1 = player2.rollDice();
+    let roll2 = player2.rollDice();
+    $("span#roll1").text(roll1);
+    $("span#roll2").text(roll2);
     $("span#player2TurnScore").text(player2.turnScore);
-
+    if(roll1 === 1 && roll2 === 1) {
+      player2.totalScore = 0;
+      // Disable player one's buttons
+      $("button.player2Btn").prop("disabled", true);
+      // Enable player two's buttons
+      $("button.player1Btn").prop("disabled", false);
+      newGame.currentPlayer = 1;
     // If player two, rolls a one, need to disable buttons
-    if (roll === 1){
+    else if (roll1 === 1 || roll2 === 1) {
       // Disable player two's buttons
       $("button.player2Btn").prop("disabled", true);
       // Enable player one's buttons
@@ -106,10 +130,10 @@ let attachEventListeners = function () {
     $("button.player2Btn").prop("disabled", true);
     // Enable player one's buttons
     $("button.player1Btn").prop("disabled", false);
-      // Change current player
+    // Change current player
     newGame.currentPlayer = 1;
   });
-  
+
 };
 
 $(document).ready(function () {
@@ -215,7 +239,7 @@ $(document).ready(function () {
 //         $("span#roll").text(rollDice2);  // Need to display individual dice rolls
 //         $("span#player2TurnScore").text(player2.turnScore);
 //       }
-    
+
 //     });
 //     $("button#player1Hold").click(function(){
 //       if(newGame.currentPlayer === 1){
@@ -230,7 +254,7 @@ $(document).ready(function () {
 //       newGame.switch();
 //     });
 //   });
-  
+
 
 
 
