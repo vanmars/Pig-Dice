da8c6b34d7082aab627a4c8ea627edc3127f757e
Vanessa Stewart <vstewart1213@gmail.com>
2020-09-02 22:10:16 -0700

    Add README.md and refactor code by eliminating commented out code and unnecessary line spacings

---
+++ README.md
@@ -0,0 +1,56 @@
+# _Pig Dice_
+
+_27 August 2020_
+
+#### _A web game to practice working with constructors and methods (for Epicodus)_
+
+#### By _**Vanessa Stewart and Daniel Schaff**_
+
+## Description
+
+_This is a Pig Dice game with options for playing with one die or two dice. Players take turns rolling a die or dice. They can decide to continue rolling to acrue potential points or "hold" and add those potential points to their score. Certain roll combinations, however, may cause a turn to end and all potential points to be lost._
+
+## Specifications
+One Die
+* Each turn, a player repeatedly rolls a die until either a 1 is rolled or the player decides to "hold":
+* If the player rolls a 1, they score nothing and it becomes the next player's turn.
+* If the player rolls any other number, it is added to their turn total and the player's turn continues.
+* If a player chooses to "hold", their turn total is added to their score, and it becomes the next player's turn.
+The first player to score 100 or more points wins.
+
+Two Dice
+* Two standard dice are rolled. If neither shows a 1, their sum is added to the turn total.
+* If a single 1 is rolled, the player scores nothing and the turn ends.
+* If two 1s are rolled, the player’s entire score is lost, and the turn ends.
+* If a double is rolled, the point total is added to the turn total as with any roll but the player is obligated to roll again (possible sub-variation of the two-dice game)
+
+## Setup/Installation Requirements
+
+To View Project:
+* Open a web browser of your choice and navigate to the following page: https://vanmars.github.io/pig-dice/
+
+To Extend This Project:
+* Clone this project using the 'git clone' command in terminal.
+* Open the cloned repo in a text editor of your choice.
+
+## Known Bugs
+
+There are no known bugs at this time.
+
+Future iterations could improve upon the visual layout and design of the screen.
+
+## Technologies Used
+
+* HTML5
+* CSS/Bootstrap
+* JavaScript/jQuery
+
+### License
+
+Copyright (c) 2020 **_Vanessa Stewart and Daniel Schaff_**
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,6 @@ function Player(playerId) {
 }
 
 let diceRoll = function () {
-  // let roll = Math.ceil(Math.random() * 6);
   let roll = Math.ceil(Math.random()*6);
   return roll;
 }
@@ -27,7 +26,6 @@ Player.prototype.rollDice = function () {
   return currentRoll;
 };
  
-
 Player.prototype.hold = function () {
   this.totalScore += this.turnScore
   this.turnScore = 0;
@@ -38,11 +36,7 @@ Player.prototype.hold = function () {
   return this.totalScore;
 };
 
-
-
-
 //User Interface Logic-------
-
 let player1 = new Player(1);
 let player2 = new Player(2);
 let newGame = new ScoreCard(player1, player2);
@@ -53,10 +47,6 @@ let attachEventListeners = function () {
     let roll = player1.rollDice();
     $("span#roll").text(roll);
     $("span#player1TurnScore").text(player1.turnScore);
-
-
-  
-
     // If player one, rolls a one, need to disable buttons
     if (roll === 1){
       // Disable player one's buttons
@@ -66,7 +56,6 @@ let attachEventListeners = function () {
       newGame.currentPlayer = 2;
     };
   });
-
   // Event Listener for Player 1 Hold
   $("button#player1Hold").click(function () {
     let score = player1.hold();
@@ -78,15 +67,12 @@ let attachEventListeners = function () {
     $("button.player2Btn").prop("disabled", false);
     // Change current player
     newGame.currentPlayer = 2;
-
   });
-
   //  Event Listener for Player 2 Roll
   $("button#player2Roll").click(function () {
     let roll = player2.rollDice();
     $("span#roll").text(roll);  
     $("span#player2TurnScore").text(player2.turnScore);
-
     // If player two, rolls a one, need to disable buttons
     if (roll === 1){
       // Disable player two's buttons
@@ -96,7 +82,6 @@ let attachEventListeners = function () {
       newGame.currentPlayer = 1;
     };
   });
-
   // Event Listener for Player 2 Hold
   $("button#player2Hold").click(function () {
     let score = player2.hold();
@@ -109,138 +94,11 @@ let attachEventListeners = function () {
       // Change current player
     newGame.currentPlayer = 1;
   });
-  
 };
-
 $(document).ready(function () {
-
-
   // Disable player two and enable player one
   $("button.player1Btn").prop("disabled", false);
   $("button.player2Btn").prop("disabled", true);
-
   attachEventListeners();
-
 });
-
-
-// // back-end logic
-
-// function ScoreCard(player1, player2) {
-//   this.players = [player1, player2];
-//   this.currentPlayer = 1;
-// }
-// //ScoreCard.prototype.addPlayer = function(player){
-// //this.players.push(player);
-// //}
-// ScoreCard.prototype.switch = function () {
-
-//   if (this.currentPlayer === 1) {
-//     this.currentPlayer = 2
-//   }
-//   else {
-//     this.currentPlayer = 1;
-
-//   }
-// }
-
-// function Player(totalScore, turnScore, playerId) {
-//   this.totalScore = totalScore;
-//   this.turnScore = turnScore;
-//   this.playerId = playerId;
-
-
-// }
-
-// let diceRoll = function () {
-//   let roll = Math.ceil(Math.random() * 6);
-//   return roll;
-// }
-
-// Player.prototype.rollDice = function () {
-//   let currentRoll = diceRoll();
-//   if (currentRoll !== 1) {
-//     this.turnScore += currentRoll;
-//   }
-//   else {
-//     this.turnScore = 0;
-//   }
-//   return currentRoll;
-
-// }
-
-// Player.prototype.hold = function () {
-//   this.totalScore += this.turnScore
-//   if (this.totalScore >= 100) {
-//     $(".winCard").show();
-//     $("span#winningPlayer").text(this.playerId)
-//   }
-//   else {
-//     this.turnScore = 0;
-//     return this.totalScore;
-//   }
-
-// }
-
-
-// //user interface logic-------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// $(document).ready(function () {
-//   let player1 = new Player(0, 0, 1);
-//   let player2 = new Player(0, 0, 2);
-//   let newGame = new ScoreCard(player1, player2);
-//   $("button#player1Roll").click(function () {
-//     if (newGame.currentPlayer === 1) {
-//       let rollDice1 = player1.rollDice();
-//       $("span#roll").text(rollDice1);
-//       $("span#player1TurnScore").text(player1.turnScore);
-//     } else {
-//         let rollDice2 = player2.rollDice();
-//         $("span#roll").text(rollDice2);  // Need to display individual dice rolls
-//         $("span#player2TurnScore").text(player2.turnScore);
-//       }
-    
-//     });
-//     $("button#player1Hold").click(function(){
-//       if(newGame.currentPlayer === 1){
-//         let hold1 = player1.hold();
-//         $("span#player1totalScore").text(hold1);
-//         $("span#player1TurnScore").text(0);
-//       }else{
-//         let hold2 = player2.hold();
-//         $("span#player2totalScore").text(hold2);
-//         $("span#player2TurnScore").text(0);
-//       }
-//       newGame.switch();
-//     });
-//   });
-  
-
-
-
-
-
-
-
-
-
-
-
-
 
